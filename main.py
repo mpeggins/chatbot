f42992f6b4ad5f3c0be6031e10622ccfea4bba29
MP <mp@orchard-ai.com>
2026-03-23 12:22:49 -0400

    Basic chatbot with API Call

---
+++ main.py
@@ -0,0 +1,22 @@
+# imports for Gemini
+from google import genai
+
+# imports for API
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+#api_key = ""
+
+client = genai.Client(api_key=api_key)
+
+# You can explore different models here:
+# for model in client.models.list():
+#     print(model.name)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-lite",
+    contents="Explain AI to me like I'm a kid.")
+
+print(response.text)
